1f711b684353139f23dc65cb53f976e19bea6213
Alex Kh. <sanchez.primal@gmail.com>
2026-03-09 11:40:00 +0100

    add scope attribute for row headers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,7 +357,7 @@ const people = [
 const tableBody = document.querySelector('table.dashboard > tbody');
 
 people.forEach(({ name: personName, sex, born, died, slug }) => {
-  const nameCell = `<th>${personName}</th>`;
+  const nameCell = `<th scope="row">${personName}</th>`;
   const genderCell = createTdHTMLString(
     sex === 'm' ? 'Male' : sex === 'f' ? 'Female' : 'Unknown',
   );
